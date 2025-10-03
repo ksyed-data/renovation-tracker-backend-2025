8b804a799a55c9af5703553db6af8040e4292120
Jin Lee <jylee3805@gmail.com>
2025-10-03 15:53:07 -0400

    CRUD routes for listings

--- src/renovation_tracker/database.py
+++ src/renovation_tracker/database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL = ''
+URL = 'mysql+pymysql://root:Renovation$tracker@localhost:3306/renovationtracker'
 
 engine = create_engine(URL)
 
 Session = sessionmaker(bind=engine, autoflush=True)
 Base = declarative_base()
-
--- src/renovation_tracker/pydantic_models/listings.py
+++ src/renovation_tracker/pydantic_models/listings.py
@@ -5,11 +5,11 @@ from typing import Optional
 
 # Schema for listing base 
 class Listing(BaseModel):
-    url: HttpUrl
+    url: str
     address: str
     description: str
     price: float
-    photos: list[str]
+ #   photos: list[str]
 
 # Schema for listing CREATE
 class ListingCreate(BaseModel):
@@ -17,14 +17,14 @@ class ListingCreate(BaseModel):
 
 # Schema for listing READ
 class ListingRead(Listing):
-    listing_id:str
+    listing_id:int
 
 # Schema for listing UPDATE
 class ListingUpdate(BaseModel):
-    listing_id: Optional[int] 
-    url: Optional[str] 
-    address: Optional[str]
-    description: Optional[str]
+    url: Optional[str] = None
+    address: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
 
 # Schema for listing DELETE
 class ListingDelete(Listing):
