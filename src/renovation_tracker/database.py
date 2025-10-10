74f3ed97a7908821c746193c66b00dafca2c67c9
Jeremiah Robert <jeremiadrjohn@gmail.com>
2025-10-10 15:40:35 -0400

    Update database.py

--- src/renovation_tracker/database.py
+++ src/renovation_tracker/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL = 'mysql+pymysql://root:Renovation$tracker@localhost:3306/renovationtracker'
+URL = 'mysql+pymysql://root:renovation_tracker2025@localhost:3306/renovation_trackerdb'
 
 engine = create_engine(URL)
 
