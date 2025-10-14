1e3ce07fee9d1d13c2ed2e30fff6192056e47314
Jin Lee <jylee3805@gmail.com>
2025-10-14 10:40:09 -0400

    fixes

--- src/renovation_tracker/database.py
+++ src/renovation_tracker/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from config import DATABASE_URL
 
-URL = 'mysql+pymysql://root:Renovation$tracker@localhost:3306/renovationtracker'
+URL = DATABASE_URL
 
 engine = create_engine(URL)
 
