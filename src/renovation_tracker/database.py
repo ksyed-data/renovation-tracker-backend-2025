a674a1ef901fc1e36ffefd8b212faf24e9fc08fd
Jin Lee <jylee3805@gmail.com>
2025-11-11 14:05:25 -0500

    db changes

--- src/renovation_tracker/database.py
+++ src/renovation_tracker/database.py
@@ -3,10 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
 load_dotenv()
 
-URL = os.getenv("DATABASE_URL", "")
+URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(URL)
 
