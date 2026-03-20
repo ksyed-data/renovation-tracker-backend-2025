775dae8de26758960e73fda50a829ec9fefbdcb7
Jin Lee <jylee3805@gmail.com>
2026-03-20 15:38:24 -0400

    changes to yolo model and schema

--- src/renovation_tracker/pydantic_models/photos.py
+++ src/renovation_tracker/pydantic_models/photos.py
@@ -6,7 +6,7 @@ from typing import Optional
 # Enum for bedrooms in photos
 class Room(StrEnum):
     LIVING_ROOM = "Livingroom"
-    DINING_ROOM = "Dinning"
+    DINING_ROOM = "Dining"
     BEDROOM = "Bedroom"
     BATHROOM = "Bathroom"
     KITCHEN = "Kitchen"
